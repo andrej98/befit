cfac83383bb10f027bc499b90248e8981ed4efac
František Bráblík <frantisek.brablik@gmail.com>
2021-12-01 15:35:27 +0100

    notification-service: Add skeleton

---
+++ notification-service/src/test/java/io/befit/NativenotificationIT.java
@@ -0,0 +1,9 @@
+package io.befit;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativenotificationIT extends notificationTest {
+
+    // Execute the same tests but in native mode.
+}
