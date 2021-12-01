114f9354d8920ba23bcbf7d569e8d5bc52b0202e
František Bráblík <frantisek.brablik@gmail.com>
2021-12-01 17:01:49 +0100

    notification-service: Add Swagger-UI / OpenAPI

--- notification-service/src/main/java/io/befit/Notification.java
+++ notification-service/src/main/java/io/befit/Notification.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
-public class notification {
+public class Notification {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
