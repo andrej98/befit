8d5b2e77191eeb6ca62ade4368b26f1e31363c5c
Andrej Valíček <andrej.valicek98@gmail.com>
2021-12-03 22:24:48 +0100

    workout service: tests finished

--- workout-service/src/test/java/org/acme/WorkoutPlanResourceTest.java
+++ workout-service/src/test/java/org/acme/WorkoutPlanResourceTest.java
@@ -1,16 +1,10 @@
 package org.acme;
 
-import io.quarkus.oidc.token.propagation.JsonWebToken;
-import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.mongodb.MongoTestResource;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.security.TestSecurity;
-import io.quarkus.test.security.oidc.Claim;
-import io.quarkus.test.security.oidc.ConfigMetadata;
-import io.quarkus.test.security.oidc.OidcSecurity;
-import io.quarkus.test.security.oidc.UserInfo;
-import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.acme.enums.AmountType;
 import org.junit.jupiter.api.Test;
 
@@ -27,42 +21,80 @@ import static org.hamcrest.CoreMatchers.is;
 public class WorkoutPlanResourceTest {
 
     @Test
-    @TestSecurity(user = "alice", roles = "user")
-    @OidcSecurity(claims = {
-            @Claim(key = "email", value = "user@gmail.com")
-    }, userinfo = {
-            @UserInfo(key = "sub", value = "subject")
-    }, config = {
-            @ConfigMetadata(key = "issuer", value = "issuer")
-    })
-    public void testCreateWorkoutPlan() {
+    @TestSecurity(user = "a@gmail.com", roles = "user")
+    public void testCreateDeleteWorkoutPlan() {
 
-        RestAssured.when().get("workout-plans").then()
-                .statusCode(200);
+        Exercise exercise = new Exercise("pushups", "chest", 10, AmountType.REPETITIONS);
+        List<Exercise> exercises = new ArrayList<>();
+        exercises.add(exercise);
+        List<DayOfWeek> days = new ArrayList<>();
+        days.add(DayOfWeek.FRIDAY);
+        days.add(DayOfWeek.SUNDAY);
+        WorkoutPlan workoutPlan = new WorkoutPlan("Plan A", exercises, days, "a@gmail.com");
 
-//        Exercise exercise = new Exercise("pushups", "chest", 10, AmountType.REPETITIONS);
-//        List<Exercise> exercises = new ArrayList<>();
-//        exercises.add(exercise);
-//        List<DayOfWeek> days = new ArrayList<>();
-//        days.add(DayOfWeek.FRIDAY);
-//        days.add(DayOfWeek.SUNDAY);
-//        WorkoutPlan workoutPlan = new WorkoutPlan("Plan A", exercises, days, "a@gmail.com");
-//
-//        Object id = given()
-//                .body(workoutPlan)
-//                .contentType("application/json")
-//                .when().post("/workout-plans")
-//                .then()
-//                .statusCode(201)
-//                .body("name", equalTo("Plan A"))
-//                .body("userName", equalTo("a@gmail.com"))
-//                .body("exercises.size()", is(1))
-//                .body("frequency.size()", is(2))
-//                .extract().path("id");
+        Object id = given()
+                .body(workoutPlan)
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .when().post("/workout-plans")
+                .then()
+                .statusCode(201)
+                .body("name", equalTo("Plan A"))
+                .body("userName", equalTo("a@gmail.com"))
+                .body("exercises.size()", is(1))
+                .body("frequency.size()", is(2))
+                .extract().path("id");
 
+        given()
+                .when().delete("/workout-plans/" + id)
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("Plan A"))
+                .body("userName", equalTo("a@gmail.com"))
+                .body("exercises.size()", is(1))
+                .body("frequency.size()", is(2))
+                .body("id", equalTo(id));
     }
 
+    @Test
+    @TestSecurity(user = "a@gmail.com", roles = "user")
+    public void testCreateUpdateWorkoutPlan() {
 
+        Exercise exercise = new Exercise("pushups", "chest", 10, AmountType.REPETITIONS);
+        List<Exercise> exercises = new ArrayList<>();
+        exercises.add(exercise);
+        List<DayOfWeek> days = new ArrayList<>();
+        days.add(DayOfWeek.FRIDAY);
+        days.add(DayOfWeek.SUNDAY);
+        WorkoutPlan workoutPlan = new WorkoutPlan("Plan C", exercises, days, "a@gmail.com");
 
+        Object id = given()
+                .body(workoutPlan)
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .when().post("/workout-plans")
+                .then()
+                .statusCode(201)
+                .body("name", equalTo("Plan C"))
+                .body("userName", equalTo("a@gmail.com"))
+                .body("exercises.size()", is(1))
+                .body("frequency.size()", is(2))
+                .extract().path("id");
+
+        WorkoutPlan updatedWorkoutPlan = new WorkoutPlan("Plan B", exercises, days, "a@gmail.com");
+
+        given()
+                .body(updatedWorkoutPlan)
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .when().put("/workout-plans/" + id)
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("Plan B"))
+                .body("userName", equalTo("a@gmail.com"))
+                .body("exercises.size()", is(1))
+                .body("frequency.size()", is(2))
+                .body("id", equalTo(id));
+    }
 
 }
