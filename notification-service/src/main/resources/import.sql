468749e8c536778499d95e6d4043929c60e7df83
František Bráblík <frantisek.brablik@gmail.com>
2021-12-04 13:45:35 +0100

    notification-service: Add working tests

--- notification-service/src/main/resources/import.sql
+++ notification-service/src/main/resources/import.sql
@@ -1,11 +1,15 @@
 INSERT INTO ScheduledRecord(id, email, exerciseName, hour, minute)
-VALUES (nextval('hibernate_sequence'), 'franta@befitorg.com', 'Pushups', 18, 0);
+VALUES (nextval('hibernate_sequence'),
+        'franta@nonexistent.com', 'Pushups', 18, 1);
 
 INSERT INTO ScheduledRecord(id, email, exerciseName, hour, minute)
-VALUES (nextval('hibernate_sequence'), 'nonexistent.email@befitorg.com', 'Pushups', 19, 0);
+VALUES (nextval('hibernate_sequence'),
+        'nonexistent.email@nonexistent.com', 'Pushups', 19, 2);
 
 INSERT INTO ScheduledRecord(id, email, exerciseName, hour, minute)
-VALUES (nextval('hibernate_sequence'), 'someone@befitorg.com', 'Running', 20, 0);
+VALUES (nextval('hibernate_sequence'),
+        'someone@nonexistent.com', 'Running', 20, 3);
 
 INSERT INTO ScheduledRecord(id, email, exerciseName, hour, minute)
-VALUES (nextval('hibernate_sequence'), 'franta@befitorg.com', 'Walking', 21, 0);
+VALUES (nextval('hibernate_sequence'),
+        'franta@nonexistent.com', 'Walking', 21, 4);
---
+++ notification-service/src/test/java/io/befit/NotificationResourceTest.java
@@ -0,0 +1,122 @@
+package io.befit;
+
+import io.quarkus.panache.common.Parameters;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
+
+import javax.ws.rs.core.Response;
+
+import static java.util.Map.entry;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.hasItem;
+
+@QuarkusTest
+public class NotificationResourceTest
+{
+    @Test
+    public void testDefaultDB()
+    {
+        given()
+            .when()
+                .get("/notifications/list")
+            .then()
+            .statusCode(200)
+            .body("size()", is(4))
+            .body("email", containsInAnyOrder("franta@nonexistent.com",
+                                             "nonexistent.email@nonexistent.com",
+                                             "someone@nonexistent.com",
+                                             "franta@nonexistent.com"))
+            .body("exerciseName", containsInAnyOrder("Pushups",
+                                                     "Pushups",
+                                                     "Running",
+                                                     "Walking"))
+            .body("hour", containsInAnyOrder(18, 19, 20, 21))
+            .body("minute", containsInAnyOrder(1, 2, 3, 4));
+    }
+
+    @Test
+    public void testEmailSearch()
+    {
+        given()
+            .when()
+                .param("email", "nonexistent.email@nonexistent.com")
+                .get("/notifications/list")
+            .then()
+            .statusCode(200)
+            .body("email", containsInAnyOrder("nonexistent.email@nonexistent.com"))
+            .body("exerciseName", containsInAnyOrder("Pushups"))
+            .body("hour", containsInAnyOrder(19))
+            .body("minute", containsInAnyOrder(2));
+
+        given()
+            .when()
+                .param("email", "franta@nonexistent.com")
+                .get("/notifications/list")
+            .then()
+            .statusCode(200)
+            .body("size()", is(2))
+            .body("email", containsInAnyOrder("franta@nonexistent.com",
+                                              "franta@nonexistent.com"));
+    }
+
+    @Test
+    public void testAddPurge()
+    {
+        String[] emails = new String[]
+        {
+            "test1@nonexistentbb.com", "test2@nonexistentcc.com",
+            "test3@nonexistentaa.com"
+        };
+
+        for (String email : emails)
+        {
+            var param = new ScheduledDTO();
+            param.time = Optional.empty();
+            param.email = email;
+            param.exerciseName = "trolololol";
+
+            var id = given()
+                .body(param)
+                    .contentType("application/json")
+                    .post("/notifications/create")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .body("email", equalTo(param.email))
+                .body("exerciseName", equalTo(param.exerciseName))
+                .extract()
+                .path("id");
+
+            given()
+                .when()
+                    .delete("/notifications/delete/" + id)
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .body("email", equalTo(param.email))
+                .body("exerciseName", equalTo(param.exerciseName))
+                .body("id", equalTo(id));
+
+            given()
+                .when()
+                    .get("/notifications/list")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .body("email", not(hasItem(email)));
+        }
+
+        given()
+            .when()
+                .delete("/notifications/purge")
+            .then()
+            .statusCode(Response.Status.NOT_FOUND.getStatusCode());
+    }
+}
+
